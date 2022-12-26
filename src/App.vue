eca004d7757f0348842c1b7ac1d58dab883c282c
Joanna <jlapa.transport@gmail.com>
2022-12-26 11:50:07 +0100

    TheHeader, App basic styles, template + script add

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
+<the-header></the-header>
 <router-view></router-view>
 </template>
 
-<style scoped>
+<script>
+import { RouterLink, RouterView } from "vue-router";
+import TheHeader from './components/layout/TheHeader.vue';
 
+export default {
+    components: {
+    TheHeader
+}
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@400;600;700&display=swap');
+
+*, *::before, *::after {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Jost', sans-serif;
+}
+
+body {
+    margin: 0 auto;
+}
 </style>
---
+++ src/router.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import FeedbackAdd from "./pages/feedbacks/FeedbackAdd.vue";
+import FeedbackDetail from "./pages/feedbacks/FeedbackDetail.vue";
+import FeedbackEdit from "./pages/feedbacks/FeedbackEdit.vue";
+import FeedbackList from "./pages/feedbacks/FeedbackList.vue";
+import StatusFeedbackList from "./pages/roadmap/StatusFeedbackList.vue";
+import NotFOund from "./pages/NotFound.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      redirect: "/feedbacks",
+    },
+    {
+      path: "/feedbacks",
+      component: FeedbackList,
+    },
+    {
+      path: "/feedbacks/:id",
+      component: FeedbackDetail,
+      children: [
+        { path: "add", component: FeedbackAdd },
+        { path: "edit", component: FeedbackEdit }, //editing feedback
+      ],
+    },
+    {
+      path: "/roadmap",
+      component: StatusFeedbackList,
+    },
+    {
+      path: "/:notFound(.*)",
+      component: NotFOund,
+    },
+  ],
+});
+
+export default router;
