9e37f82ae106ff0a420d23e22eaa18081142e1a7
Joanna <jlapa.transport@gmail.com>
2022-12-28 18:34:11 +0100

    DropDownMenu moved to ui folder

--- src/components/layout/DropdownMenu.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <label>Sort by:</label>
-    <select name="choice">
-        <option value="most upvotes">Most upvotes</option>
-        <option value="least upvotes">Least upvotes</option>
-        <option value="most comments">Most comments</option>
-        <option value="least comments">Least comments</option>
-    </select>
-</template>
