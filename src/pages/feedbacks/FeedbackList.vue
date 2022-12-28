871b9f1ad75ce249dcb94c34c1ab5bbdbdb773f5
Joanna <jlapa.transport@gmail.com>
2022-12-28 18:33:07 +0100

    FeedBackList component sceleton part made

--- src/pages/feedbacks/FeedbackList.vue
+++ src/pages/feedbacks/FeedbackList.vue
@@ -1,13 +1,22 @@
 <template>
   <the-header></the-header>
+  <blue-bar>
+    <suggestions></suggestions>
+    <dropdown-menu></dropdown-menu>
+  </blue-bar>
 </template>
 <script>
 import TheHeader from "../../components/layout/TheHeader.vue";
-
+import BlueBar from "../../components/ui/BlueBar.vue";
+import Suggestions from "../../components/ui/Suggestions.vue";
+import DropdownMenu from "../../components/ui/DropdownMenu.vue";
 
 export default {
   components: {
     TheHeader,
+    BlueBar,
+    Suggestions,
+    DropdownMenu,
   },
 };
 </script>
