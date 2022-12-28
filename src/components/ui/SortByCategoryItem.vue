615dd8a6f3f0f046e4680be5f5df819e6942e3c7
Joanna <jlapa.transport@gmail.com>
2022-12-28 18:34:51 +0100

    SortByCategoryItem component sceleton partly made

---
+++ src/components/ui/SortByCategoryItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <base-button>
+      <slot></slot>
+    </base-button>
+  </li>
+</template>
+
+<script>
+import BaseButton from "./BaseButton.vue";
+
+export default {
+  components: {
+    BaseButton,
+  },
+  props: {
+    categories: ["All", "UI", "UX", "Enhancement", "Feature", "Bug"],
+  },
+};
+</script>
