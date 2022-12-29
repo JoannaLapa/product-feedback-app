4e6319a52776a6914eca1781b12a2d576925a2bd
Joanna <jlapa.transport@gmail.com>
2022-12-29 11:11:17 +0100

    bg-color add

--- src/components/ui/BlueBar.vue
+++ src/components/ui/BlueBar.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="card">
+  <div class="blue-bar__card">
     <slot></slot>
   </div>
 </template>
-<style lang="scss"></style>
+
+<style lang="scss">
+.blue-bar__card {
+background-color: #373F68;
+// bug to fix - there should be a global variable, I need to check the configuration of scss, if vite accept a new way of files organisation in scss (@forward and @use) - for this moment my configuration doesn't work so I am going to finish the sceleton and I'll check this problem later.
+}
+</style>
