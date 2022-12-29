73aecf1a8240271ef07f7b614b5da1794ca6ff71
Joanna <jlapa.transport@gmail.com>
2022-12-29 13:12:20 +0100

    SortByCategoryItem component add

---
+++ src/components/ui/TheHeaderMenu/sortByCategory/SortByCategoryItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <base-button>Test</base-button>
+  </li>
+</template>
+
+<script>
+import BaseButton from "../../BaseButton.vue";
+
+export default {
+  components: {
+    BaseButton,
+  },
+  props: {
+    categories: ["All", "UI", "UX", "Enhancement", "Feature", "Bug"],
+  },
+};
+</script>
