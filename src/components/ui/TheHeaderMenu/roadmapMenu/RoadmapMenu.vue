e8b35851780ba223b996a9ec3783acc586a35ab5
Joanna <jlapa.transport@gmail.com>
2022-12-29 13:14:15 +0100

    RoadmapMenu component add

---
+++ src/components/ui/TheHeaderMenu/roadmapMenu/RoadmapMenu.vue
@@ -0,0 +1,27 @@
+<template>
+    <li>
+    <ul>
+      Roadmap
+      <router-link to="/roadmap">View</router-link>
+      <li>
+        Planned
+        <!-- <span>{{ statusName }}</span> -->
+      </li>
+      <li>
+        In-Progress
+        <!-- <span>{{ statusName }}</span> -->
+      </li>
+      <li>
+        Live
+        <!-- <span>{{ statusName }}</span> -->
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+//   props: [statusName],
+}
+//idea for future solution: check the index of separate status and make a method for calculating the number of them if "planned" i++ etc.
+</script>
