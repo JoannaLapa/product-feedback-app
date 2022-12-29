65fa915a59644dfbb17addd71d496e58df02e88a
Joanna <jlapa.transport@gmail.com>
2022-12-29 16:09:04 +0100

    CommentsList component add

---
+++ src/components/comments/CommentsList.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <base-box>
+      <h2>
+        <span>{{}}</span>Comments
+      </h2>
+      <!-- if there is only one comments we should write in singular form -->
+      <div>
+        <ul>
+          <comment-item></comment-item>
+          <!-- add inside comment-item v-if if there is a reply -->
+        </ul>
+      </div>
+    </base-box>
+  </section>
+</template>
+
+<script>
+import CommentItem from './CommentItem.vue';
+
+export default {
+  components: {
+    CommentItem,
+  }
+}
+</script>
