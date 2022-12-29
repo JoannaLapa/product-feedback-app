878c3df721e42ce68c4cb962d33d2cd09389d651
Joanna <jlapa.transport@gmail.com>
2022-12-29 16:10:33 +0100

    BaseBox component attach

--- src/components/ui/TheHeaderMenu/roadmapMenu/RoadmapMenu.vue
+++ src/components/ui/TheHeaderMenu/roadmapMenu/RoadmapMenu.vue
@@ -1,27 +1,34 @@
-<template>
-    <li>
-    <ul>
-      Roadmap
-      <router-link to="/roadmap">View</router-link>
-      <li>
-        Planned
-        <!-- <span>{{ statusName }}</span> -->
-      </li>
-      <li>
-        In-Progress
-        <!-- <span>{{ statusName }}</span> -->
-      </li>
-      <li>
-        Live
-        <!-- <span>{{ statusName }}</span> -->
-      </li>
-    </ul>
+<temnplate>
+  <li>
+    <base-box>
+      <ul>
+        Roadmap
+        <router-link to="/roadmap">View</router-link>
+        <li>
+          Planned
+          <!-- <span>{{ statusName }}</span> -->
+        </li>
+        <li>
+          In-Progress
+          <!-- <span>{{ statusName }}</span> -->
+        </li>
+        <li>
+          Live
+          <!-- <span>{{ statusName }}</span> -->
+        </li>
+      </ul>
+    </base-box>
   </li>
-</template>
+</temnplate>
 
 <script>
+import BaseBox from "../BaseBox.vue";
+
 export default {
-//   props: [statusName],
-}
+  components: {
+    BaseBox,
+  },
+  //   props: [statusName],
+};
 //idea for future solution: check the index of separate status and make a method for calculating the number of them if "planned" i++ etc.
 </script>
