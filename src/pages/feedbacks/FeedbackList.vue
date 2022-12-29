083c07000eb255d52c3a23c8f968f5b8d37967ea
Joanna <jlapa.transport@gmail.com>
2022-12-29 13:18:26 +0100

    TheMain, BaeButton components attach

--- src/pages/feedbacks/FeedbackList.vue
+++ src/pages/feedbacks/FeedbackList.vue
@@ -1,12 +1,17 @@
 <template>
   <the-header></the-header>
+  <the-main>
   <blue-bar>
     <suggestions></suggestions>
     <dropdown-menu></dropdown-menu>
+    <base-button>+ Add Feedback</base-button>
   </blue-bar>
+</the-main>
 </template>
 <script>
 import TheHeader from "../../components/layout/TheHeader.vue";
+import TheMain from "../../components/layout/TheMain.vue";
+import BaseButton from "../../components/ui/BaseButton.vue";
 import BlueBar from "../../components/ui/BlueBar.vue";
 import Suggestions from "../../components/ui/Suggestions.vue";
 import DropdownMenu from "../../components/ui/DropdownMenu.vue";
@@ -14,6 +19,8 @@ import DropdownMenu from "../../components/ui/DropdownMenu.vue";
 export default {
   components: {
     TheHeader,
+    TheMain,
+    BaseButton,
     BlueBar,
     Suggestions,
     DropdownMenu,
