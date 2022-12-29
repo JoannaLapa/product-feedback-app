23d02ba9ee0091e264778e58eec72d1278cae607
Joanna <jlapa.transport@gmail.com>
2022-12-29 16:10:49 +0100

    BaseBox component attach

--- src/components/ui/TheHeaderMenu/sortByCategory/SortByCategory.vue
+++ src/components/ui/TheHeaderMenu/sortByCategory/SortByCategory.vue
@@ -1,20 +1,24 @@
 <template>
-<li>
-    <ul>
-      <sort-by-category-item></sort-by-category-item>
-      <!-- Above component for the buttons of categories -->
-      <!-- add v-for="category in categories"-->
-      <!-- It doesn't work, I'm going to fix it later - I want to use v-for and attach category dynamically-->
-    </ul>
+  <li>
+    <base-box>
+      <ul>
+        <sort-by-category-item></sort-by-category-item>
+        <!-- Above component for the buttons of categories -->
+        <!-- add v-for="category in categories"-->
+        <!-- It doesn't work, I'm going to fix it later - I want to use v-for and attach category dynamically-->
+      </ul>
+    </base-box>
   </li>
 </template>
 
 <script>
+import BaseBox from "../BaseBox.vue";
 import SortByCategoryItem from "./SortByCategoryItem.vue";
 
 export default {
-    components: {
-        SortByCategoryItem,
-    }
-}
+  components: {
+    BaseBox,
+    SortByCategoryItem,
+  },
+};
 </script>
