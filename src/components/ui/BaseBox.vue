b22ecc1cb29daa17fc0c4d16f503779b855c4717
Joanna <jlapa.transport@gmail.com>
2022-12-29 14:18:23 +0100

    BaseBox component add

---
+++ src/components/ui/BaseBox.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="base-box">
+    <slot></slot>
+    </div>
+</template>
+
+<style lang="scss">
+.base-box {
+    border-radius: 0.625rem;
+    background-color: #FFFFFF;
+}
+</style>
