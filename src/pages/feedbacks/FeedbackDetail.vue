e9864217a9555181efa4e43c0a48391a09e9ddb4
Joanna <jlapa.transport@gmail.com>
2022-12-29 16:12:14 +0100

    GoBack, BaseButton, FeedbackItem component attach

--- src/pages/feedbacks/FeedbackDetail.vue
+++ src/pages/feedbacks/FeedbackDetail.vue
@@ -1,2 +1,22 @@
-<template></template>
-<script></script>
+<template>
+  <div>
+    <go-back>
+      <base-button>Edit Feedback</base-button>
+    </go-back>
+    <feedback-item></feedback-item>
+  </div>
+</template>
+
+<script>
+import BaseButton from "../../components/ui/BaseButton.vue";
+import GoBack from "../../components/ui/GoBack.vue";
+import FeedbackItem from "../../components/feedbacks/FeedBackItem.vue";
+
+export default {
+  components: {
+    BaseButton,
+    GoBack,
+    FeedbackItem,
+  },
+};
+</script>
