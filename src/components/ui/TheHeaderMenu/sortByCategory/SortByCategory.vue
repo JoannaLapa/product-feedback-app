c0e9ea4ba4bb8ff1881cce5d3ee561a7f5b8a536
Joanna <jlapa.transport@gmail.com>
2022-12-29 13:12:42 +0100

    SortByCategory component add

---
+++ src/components/ui/TheHeaderMenu/sortByCategory/SortByCategory.vue
@@ -0,0 +1,20 @@
+<template>
+<li>
+    <ul>
+      <sort-by-category-item></sort-by-category-item>
+      <!-- Above component for the buttons of categories -->
+      <!-- add v-for="category in categories"-->
+      <!-- It doesn't work, I'm going to fix it later - I want to use v-for and attach category dynamically-->
+    </ul>
+  </li>
+</template>
+
+<script>
+import SortByCategoryItem from "./SortByCategoryItem.vue";
+
+export default {
+    components: {
+        SortByCategoryItem,
+    }
+}
+</script>
