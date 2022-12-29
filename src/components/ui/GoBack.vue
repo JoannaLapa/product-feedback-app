2eb099d2b09ed51af4eaa917fbaae91774868e73
Joanna <jlapa.transport@gmail.com>
2022-12-29 14:35:16 +0100

    GoBack component add

---
+++ src/components/ui/GoBack.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="go-back__container">
+    <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M6 9L2 5l4-4"
+        stroke="#4661E6"
+        stroke-width="2"
+        fill="none"
+        fill-rule="evenodd"
+      />
+    </svg>
+    <router-link to>Go Back</router-link>
+  </div>
+  <slot></slot>
+</template>
+
+<script>
+export default {
+    props: {
+        to: {
+            type: String,
+            reqired: false,
+            default: '/feedbacks',   
+        }
+    }
+}
+</script>
