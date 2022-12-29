ffc87089cf02e297ebdd5bbf17010adcc415b8f7
Joanna <jlapa.transport@gmail.com>
2022-12-29 13:17:35 +0100

    TheMain component add

---
+++ src/components/layout/TheMain.vue
@@ -0,0 +1,16 @@
+<template>
+  <slot></slot>
+  <wrapper></wrapper>
+</template>
+
+<script>
+import Wrapper from "../ui/Wrapper.vue";
+
+export default {
+  components: {
+    Wrapper,
+  },
+};
+</script>
+
+<style lang="scss"></style>
