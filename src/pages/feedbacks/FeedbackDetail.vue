68414878ea45eee1315b2bf534f24168dc12cb57
Joanna <jlapa.transport@gmail.com>
2022-12-30 20:12:44 +0100

    CommentList & AddComment component add code format

--- src/pages/feedbacks/FeedbackDetail.vue
+++ src/pages/feedbacks/FeedbackDetail.vue
@@ -4,6 +4,8 @@
       <base-button>Edit Feedback</base-button>
     </go-back>
     <feedback-item></feedback-item>
+    <comments-list></comments-list>
+    <add-comment></add-comment>
   </div>
 </template>
 
@@ -11,12 +13,16 @@
 import BaseButton from "../../components/ui/BaseButton.vue";
 import GoBack from "../../components/ui/GoBack.vue";
 import FeedbackItem from "../../components/feedbacks/FeedBackItem.vue";
+import CommentsList from "../../components/comments/CommentsList.vue";
+import AddComment from "../../components/comments/AddComment.vue";
 
 export default {
   components: {
     BaseButton,
     GoBack,
     FeedbackItem,
+    CommentsList,
+    AddComment,
   },
 };
 </script>
