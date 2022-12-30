54610284b397f8574f48369d94d51144c57df01b
Joanna <jlapa.transport@gmail.com>
2022-12-30 19:22:02 +0100

    AddComment component add

---
+++ src/components/comments/AddComment.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <fieldset>
+      <legend>Add comment:</legend>
+      <textarea
+        id="comment"
+        name="comment"
+        maxlength="250"
+        placeholder="Type your comment here"
+      >
+      </textarea>
+      <label for="comment">Dynamic number characters left</label>
+    </fieldset>
+  </form>
+</template>
+<base-button>Post comment</base-button>
+<!-- future styling textarea:valid/invalid for active states possible on textarea and form -->
+<!-- check how to add dynamic number of characters left in the input -->
+<script>
+import BaseButton from "../ui/BaseButton.vue";
+
+export default {
+  components: {
+    BaseButton,
+  },
+};
+</script>
