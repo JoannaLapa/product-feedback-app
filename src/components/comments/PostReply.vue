a5725572d6d1d9ed11fe7f3d01e1023f984f90ee
Joanna <jlapa.transport@gmail.com>
2022-12-30 20:12:01 +0100

    basic sceleton make

---
+++ src/components/comments/PostReply.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <textarea maxlength="250" name="reply" id="comment"></textarea>
+    <base-button>Post Reply</base-button>
+  </form>
+</template>
+
+<script>
+import BaseButton from "../ui/BaseButton.vue";
+
+export default {
+  components: {
+    BaseButton,
+  },
+};
+</script>
