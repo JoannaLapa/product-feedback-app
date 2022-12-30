9d19a31541de5cf7667ba6e3d1d28916ff558ca2
Joanna <jlapa.transport@gmail.com>
2022-12-30 20:11:38 +0100

    PotReply component and comment add, code format

--- src/components/comments/CommentsList.vue
+++ src/components/comments/CommentsList.vue
@@ -1,14 +1,15 @@
 <template>
   <section>
     <base-box>
-      <h2>
-        <span>{{}}</span>Comments
-      </h2>
+      <h2><span>{{}}</span>Comments</h2>
       <!-- if there is only one comments we should write in singular form -->
       <div>
         <ul>
-          <comment-item></comment-item>
+          <comment-item>
+            <post-reply></post-reply>
+          </comment-item>
           <!-- add inside comment-item v-if if there is a reply -->
+          <!-- add post-replay when the user clicks on reply -->
         </ul>
       </div>
     </base-box>
@@ -16,11 +17,13 @@
 </template>
 
 <script>
-import CommentItem from './CommentItem.vue';
+import CommentItem from "./CommentItem.vue";
+import PostReply from "./PostReply.vue";
 
 export default {
   components: {
     CommentItem,
-  }
-}
+    PostReply,
+  },
+};
 </script>
