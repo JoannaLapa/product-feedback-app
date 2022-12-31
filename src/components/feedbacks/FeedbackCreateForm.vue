9aa52359eb8b4a3a0fda2f4c9b9997fd42c24054
Joanna <jlapa.transport@gmail.com>
2022-12-31 16:44:36 +0100

    component sceleton finish

---
+++ src/components/feedbacks/FeedbackCreateForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <form>
+    <fieldset>
+      <legend>{{ (title, dynamicTitle) }}</legend>
+      <label for="feedback-title">Feedback Title</label>
+      <p id="feedback-title-instruction">Add a short, descriptive headline</p>
+      <input
+        type="text"
+        id="feedback-title"
+        aria-describedby="feedback-title-instruction"
+      />
+      <label for="category">Category</label>
+      <p id="category-instruction">Choose a category for your feedback</p>
+      <select id="category" aria-describedby="category-instruction">
+        <option value="Feature">Feature</option>
+        <option value="UI">UI</option>
+        <option value="UX">UX</option>
+        <option value="Enhancement">Enhancement</option>
+        <option value="Bug">Bug</option>
+      </select>
+      <label for="feedback-detail">Feedback Detail</label>
+      <p id="feedback-details-instruction">
+        Include any specific comments on what should be improved, added, etc.
+      </p>
+      <textarea
+        id="feedback-detail"
+        aria-describedby="feedback-details-instruction"
+        name="feedback-detail"
+      >
+      </textarea>
+    </fieldset>
+    <base-button v-if="edit">{{ delete }}</base-button>
+    <base-button>Cancel</base-button>
+    <base-button>{{ submitTitle }}</base-button>
+  </form>
+</template>
+
+<script>
+import BaseButton from "../ui/BaseButton.vue";
+
+export default {
+  components: {
+    BaseButton,
+  },
+  props: ["title", "edit", "delete", "submitTitle", "dynamicTitle"],
+  //   computed: {
+  //     dynamicTitle() {
+  //         this.dynamicTitle = 'attachTitleFromJson'
+  //     }
+  //   }
+};
+</script>
