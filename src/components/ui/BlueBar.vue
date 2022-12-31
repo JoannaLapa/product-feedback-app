71aa13a928e5921620ac1ff695222efc20dbf941
Joanna <jlapa.transport@gmail.com>
2022-12-31 16:42:29 +0100

    code format

--- src/components/ui/BlueBar.vue
+++ src/components/ui/BlueBar.vue
@@ -6,7 +6,7 @@
 
 <style lang="scss">
 .blue-bar__card {
-background-color: #373F68;
-// bug to fix - there should be a global variable, I need to check the configuration of scss, if vite accept a new way of files organisation in scss (@forward and @use) - for this moment my configuration doesn't work so I am going to finish the sceleton and I'll check this problem later.
+  background-color: #373f68;
+  // bug to fix - there should be a global variable, I need to check the configuration of scss, if vite accept a new way of files organisation in scss (@forward and @use) - for this moment my configuration doesn't work so I am going to finish the sceleton and I'll check this problem later.
 }
 </style>
