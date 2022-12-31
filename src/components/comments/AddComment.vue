3aac5b38dad88c47347711ea6b8700b17e544e06
Joanna <jlapa.transport@gmail.com>
2022-12-31 16:41:17 +0100

    component sceleton finish

--- src/components/comments/AddComment.vue
+++ src/components/comments/AddComment.vue
@@ -1,27 +1,31 @@
 <template>
-  <form>
-    <fieldset>
+  <base-box>
+    <form>
       <label for="comment">Add comment:</label>
       <textarea
         id="comment"
         name="comment"
         maxlength="250"
         placeholder="Type your comment here"
+        aria-describedby="characters-left"
       >
       </textarea>
-      <legend>Dynamic number characters left</legend>
-    </fieldset>
-  </form>
+      <p id="characters-left">Dynamic number characters left</p>
+      <base-button>Post comment</base-button>
+    </form>
+  </base-box>
 </template>
-<base-button>Post comment</base-button>
+
 <!-- future styling textarea:valid/invalid for active states possible on textarea and form -->
 <!-- check how to add dynamic number of characters left in the input -->
 <script>
 import BaseButton from "../ui/BaseButton.vue";
+import BaseBox from "../ui/BaseBox.vue";
 
 export default {
   components: {
     BaseButton,
+    BaseBox,
   },
 };
 </script>
