594215b565321bed274236161d67cfb184ad9c69
Joanna <jlapa.transport@gmail.com>
2023-01-01 13:00:15 +0100

    comment add

--- src/components/ui/DropdownMenu.vue
+++ src/components/ui/DropdownMenu.vue
@@ -1,5 +1,6 @@
 <template>
   <label>Sort by:</label>
+  <!-- extrenal library for Select styling?? Icon up and down should be added -->
   <select name="choice">
     <option value="most upvotes">Most upvotes</option>
     <option value="least upvotes">Least upvotes</option>
