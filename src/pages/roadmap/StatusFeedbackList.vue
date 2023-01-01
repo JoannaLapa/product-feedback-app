c1dc580fca65433958bfaee2ff737f9676bde602
Joanna <jlapa.transport@gmail.com>
2023-01-01 14:01:38 +0100

    basic sceleton finish

--- src/pages/roadmap/StatusFeedbackList.vue
+++ src/pages/roadmap/StatusFeedbackList.vue
@@ -1,2 +1,44 @@
-<template></template>
-<script></script>
+<template>
+  <main>
+    <wrapper>
+      <blue-bar>
+        <go-back></go-back>
+        <h2>Roadmap</h2>
+        <base-button>+ Add Feedback</base-button>
+      </blue-bar>
+    </wrapper>
+    <wrapper>
+      <ul>
+        <li>
+            <!-- list of items dependly on status name -->
+          <h3></h3>
+          <!-- status name here, use v-for to attach the list, h3 name and description below dependly on the status name -->
+          <p>{{ description }}</p>
+          <ul>
+            <li>
+              <feedback-item></feedback-item>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </wrapper>
+  </main>
+</template>
+
+<script>
+import Wrapper from "../../components/ui/Wrapper.vue";
+import BlueBar from "../../components/ui/BlueBar.vue";
+import GoBack from "../../components/ui/GoBack.vue";
+import BaseButton from "../../components/ui/BaseButton.vue";
+import FeedbackItem from "../../components/feedbacks/FeedbackItem.vue";
+
+export default {
+  components: {
+    Wrapper,
+    BlueBar,
+    GoBack,
+    BaseButton,
+    FeedbackItem,
+  },
+};
+</script>
