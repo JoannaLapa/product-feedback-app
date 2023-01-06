867b6b88a37c7df49bf4d06ca426b7c0ef6f62e1
Joanna <jlapa.transport@gmail.com>
2023-01-06 17:32:13 +0100

    code format, self-closing tag set

--- src/components/comments/CommentsList.vue
+++ src/components/comments/CommentsList.vue
@@ -1,12 +1,15 @@
 <template>
   <section>
     <BaseBox>
-      <h2><span>{{}}</span>Comments</h2>
+      <h2>
+        <span>{{}}</span>
+        Comments
+      </h2>
       <!-- if there is only one comments we should write in singular form -->
       <div>
         <ul>
           <CommentItem>
-            <PostReply></PostReply>
+            <PostReply />
           </CommentItem>
           <!-- add inside comment-item v-if if there is a reply -->
           <!-- add post-replay when the user clicks on reply -->
