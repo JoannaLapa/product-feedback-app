12e651f6741215302cf8229cd31ab6df5725cc9f
Joanna <jlapa.transport@gmail.com>
2023-01-06 20:55:17 +0100

    scoped deleted from styles

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 import { RouterLink, RouterView } from "vue-router";
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Jost:wght@400;600;700&display=swap");
 
 *,
