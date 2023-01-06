e3db4699586c910c3a68188ac572c422b289c1c4
Joanna <jlapa.transport@gmail.com>
2023-01-06 14:47:46 +0100

    template tag add

--- src/components/icons/ArrowLeft.vue
+++ src/components/icons/ArrowLeft.vue
@@ -1,4 +1,4 @@
-<svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+<template><svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M6 9L2 5l4-4"
       stroke="#4661E6"
@@ -7,3 +7,4 @@
       fill-rule="evenodd"
     />
   </svg>
+</template>
