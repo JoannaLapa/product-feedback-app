6ead70e9ff0edc677bf8d115da6c6ee4a1b0be3c
Joanna <jlapa.transport@gmail.com>
2023-01-08 17:45:26 +0100

    DropdownMenu replaced with BaseSelect & BaseOption

--- src/pages/feedbacks/FeedbackList.vue
+++ src/pages/feedbacks/FeedbackList.vue
@@ -4,7 +4,15 @@
     <Wrapper>
       <BaseBar color="blue">
         <ShowSuggestions />
-        <DropdownMenu />
+        <label>Sort by:</label>
+        <BaseSelect name="choice">
+          <BaseOption
+            v-for="optionDetail in optionDetails"
+            :key="optionDetail"
+            :value="optionDetail"
+            :text="optionDetail"
+          />
+        </BaseSelect>
         <BaseButton link to="/add">+ Add Feedback</BaseButton>
       </BaseBar>
       <NoFeedback v-if="noFeedback" />
@@ -16,11 +24,17 @@
 import TheHeader from "../../components/basicComponents/TheHeader.vue";
 import BaseButton from "../../components/basicComponents/BaseButton.vue";
 import BaseBar from "../../components/basicComponents/BaseBar.vue";
+import BaseOption from "../../components/basicComponents/BaseOption.vue";
+import BaseSelect from "../../components/basicComponents/BaseSelect.vue";
 import ShowSuggestions from "../../components/basicComponents/ShowSuggestions.vue";
-import DropdownMenu from "../../components/basicComponents/DropdownMenu.vue";
 import FeedbackItem from "../../components/feedbacks/FeedbackItem.vue";
 import NoFeedback from "../../components/feedbacks/NoFeedback.vue";
 import Wrapper from "../../components/basicComponents/Wrapper.vue";
 // set the v-if - if there is no feedback add NoFeedback component v-else if there are feedbacks set FeedbackItem component with v-for
-// props: ["noFeedback"],
+const optionDetails = [
+  "Most Upvotes",
+  "Least Upvotes",
+  "Most Comments",
+  "Least Comments",
+];
 </script>
