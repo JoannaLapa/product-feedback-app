420f6efe010d839319644cf00b135319f7416342
Joanna <jlapa.transport@gmail.com>
2023-01-11 09:45:44 +0100

    feedbacks object, dynamic suggestions number add

--- src/components/basicComponents/ShowSuggestions.vue
+++ src/components/basicComponents/ShowSuggestions.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
     <IconSuggestions />
-    <p>
-      <span>dynamic number here</span>
+    <p v-if="Object.keys(feedbacks).length !== 1">
+      <span>{{ Object.keys(feedbacks).length }}</span>
       Suggestions
+    </p>
+    <p v-else>
+      <span>{{ Object.keys(feedbacks).length }}</span> Suggestion
     </p>
   </div>
 </template>
 
 <script setup>
 import IconSuggestions from "../icons/IconSuggestions.vue";
+
+defineProps({
+  feedbacks: {
+    type: Object,
+    default: null,
+  },
+});
 </script>
