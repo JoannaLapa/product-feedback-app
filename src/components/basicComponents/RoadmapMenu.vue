086dc9cbca3e46edf10f92e729f3fdf232c75b74
Joanna <jlapa.transport@gmail.com>
2023-01-11 12:32:02 +0100

    comment for future logic

--- src/components/basicComponents/RoadmapMenu.vue
+++ src/components/basicComponents/RoadmapMenu.vue
@@ -24,4 +24,13 @@
 <script setup>
 import BaseBox from "./BaseBox.vue";
 //idea for future solution: check the index of separate status and make a method for calculating the number of them if "planned" i++ etc.
+
+defineProps({
+  feedbacks: {
+    type: Object,
+    default: null,
+  },
+});
+
+//musimy wyciąnąć wszystkie wartości z kluczy status i następnie posortować - jeśli feedback.status.value === "live" dodaj do tablicy i na koncu zwróć długość tablicy
 </script>
