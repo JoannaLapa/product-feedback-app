7781ae4882cf219e3b5a5f30172c422c4e72b2c3
Joanna <jlapa.transport@gmail.com>
2023-01-13 00:13:37 +0100

    RoadmapLi component attach

--- src/components/basicComponents/RoadmapMenu.vue
+++ src/components/basicComponents/RoadmapMenu.vue
@@ -4,33 +4,23 @@
       <ul>
         Roadmap
         <router-link to="/roadmap">View</router-link>
-        <li>
-          Planned
-          <!-- <span>{{ statusName }}</span> -->
-        </li>
-        <li>
-          In-Progress
-          <!-- <span>{{ statusName }}</span> -->
-        </li>
-        <li>
-          Live
-          <!-- <span>{{ statusName }}</span> -->
-        </li>
+        <RoadmapLi
+          v-for="[statusName, statusNumber] in COUNTED_STATUS_MAP"
+          :key="statusName"
+          :status-name="statusName"
+          :status-number="statusNumber"
+        />
       </ul>
     </BaseBox>
   </li>
 </template>
 
 <script setup>
+import { computed } from "vue";
+import { useFeedbacksStore } from "../../stores/feedbacks";
 import BaseBox from "./BaseBox.vue";
-//idea for future solution: check the index of separate status and make a method for calculating the number of them if "planned" i++ etc.
+import RoadmapLi from "./RoadmapLi.vue";
 
-defineProps({
-  feedbacks: {
-    type: Object,
-    default: null,
-  },
-});
-
-//musimy wyciąnąć wszystkie wartości z kluczy status i następnie posortować - jeśli feedback.status.value === "live" dodaj do tablicy i na koncu zwróć długość tablicy
+const feedbacksStore = useFeedbacksStore();
+const COUNTED_STATUS_MAP = computed(() => feedbacksStore.COUNTED_STATUS_MAP);
 </script>
