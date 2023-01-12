db63e785e49eca075fca1851c957ea966515aa75
Joanna <jlapa.transport@gmail.com>
2023-01-13 00:12:45 +0100

    new component create

---
+++ src/components/basicComponents/RoadmapLi.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    {{ statusName }}
+    <span>{{ statusNumber }}</span>
+  </li>
+</template>
+
+<script setup>
+defineProps({
+  statusName: {
+    type: String,
+    required: true,
+  },
+  statusNumber: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
