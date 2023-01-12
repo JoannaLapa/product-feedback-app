53337d83afa6d4474b3eea8af6060eccdda19e0d
Joanna <jlapa.transport@gmail.com>
2023-01-12 22:47:39 +0100

    pinia add

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "@/App.vue";
-import router from "./router.js";
+import router from "@/router.js";
 
-// import "./assets/main.css";
+const pinia = createPinia();
 
-const app = createApp(App);
-
-app.use(router);
-
-app.mount("#app");
+createApp(App).use(pinia).use(router).mount("#app");
