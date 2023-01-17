9c3f85a32e34726f8056287eacefc623c666f6ab
Joanna <jlapa.transport@gmail.com>
2023-01-17 01:34:00 +0100

    COUNT_UPVOTES action & upvotes state add

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,13 +1,18 @@
 import { defineStore } from "pinia";
 
 export const ADD_SELECTED_CATEGORY = "ADD_SELECTED_CATEGORY";
+export const COUNT_UPVOTES = "COUNT_UPVOTES";
 export const useUserStore = defineStore("user", {
   state: () => ({
     selectedCategories: [],
+    upvotes: "",
   }),
   actions: {
     [ADD_SELECTED_CATEGORY](category) {
       this.selectedCategories = category;
+    },
+    [COUNT_UPVOTES](upvotes) {
+      this.upvotes = upvotes;
     },
   },
 });
