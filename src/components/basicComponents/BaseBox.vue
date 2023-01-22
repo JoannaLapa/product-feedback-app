8d37daa342023546c9d2fe2452c77905e36463e2
Joanna <jlapa.transport@gmail.com>
2023-01-22 18:06:25 +0100

    variant props added

--- src/components/basicComponents/BaseBox.vue
+++ src/components/basicComponents/BaseBox.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="rounded-lg bg-neutral-100">
+  <div
+    class="rounded-lg bg-neutral-100"
+    :class="{
+      'pl-6 pt-6 pb-9 pr-4': variant === 'small',
+    }"
+  >
     <slot></slot>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  variant: {
+    type: String,
+    default: " ",
+    required: false,
+    validation: (variant) => ["small"].includes(variant),
+  },
+});
+</script>
