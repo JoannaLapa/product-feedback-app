532e02ad420e913ed4afe3656727281e73c49911
Joanna <jlapa.transport@gmail.com>
2023-01-24 19:47:44 +0100

    lg styles updated

--- src/components/basicComponents/BaseWrapper.vue
+++ src/components/basicComponents/BaseWrapper.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="my-0 mx-auto w-screen sm:max-w-[43.0625rem] sm:px-10 md:px-0">
+  <div
+    class="my-0 mx-auto w-screen sm:max-w-[43.0625rem] sm:px-10 md:px-0 lg:my-[5.875rem] lg:flex lg:max-w-[67.5rem] lg:gap-[1.875rem]"
+  >
     <slot></slot>
   </div>
 </template>
