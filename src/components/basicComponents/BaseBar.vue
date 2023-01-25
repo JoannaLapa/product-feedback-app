3f06cf2df0bf564dd519dc45b2560e92f408af10
Joanna <jlapa.transport@gmail.com>
2023-01-25 12:22:51 +0100

    variant props added, unnecessary code deleted

--- src/components/basicComponents/BaseBar.vue
+++ src/components/basicComponents/BaseBar.vue
@@ -1,24 +1,20 @@
 <template>
-  <div :class="color">
+  <div
+    class="flex"
+    :class="{
+      'min-h-[3.5rem] bg-neutral-600': variant === 'primary',
+    }"
+  >
     <slot></slot>
   </div>
 </template>
 
 <script setup>
 defineProps({
-  color: {
+  variant: {
     type: String,
-    default: "blue",
+    required: true,
+    validation: (variant) => ["primary", "neutral"].includes(variant),
   },
 });
 </script>
-
-<style scoped>
-.blue {
-  background-color: #373f68;
-  /* // bug to fix - there should be a global variable, I need to check the configuration of scss, if vite accept a new way of files organisation in scss (@forward and @use) - for this moment my configuration doesn't work so I am going to finish the sceleton and I'll check this problem later. */
-}
-.white {
-  background-color: #ffffff;
-}
-</style>
