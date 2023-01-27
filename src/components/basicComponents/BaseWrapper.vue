c8623840542b0594290695973fb893c55f93fe88
Joanna <jlapa.transport@gmail.com>
2023-01-27 20:19:43 +0100

    max-w added

--- src/components/basicComponents/BaseWrapper.vue
+++ src/components/basicComponents/BaseWrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative my-0 mx-auto w-screen sm:max-w-[43.0625rem] sm:px-10 md:px-0 lg:my-[5.875rem] lg:flex lg:max-w-[67.5rem] lg:gap-[1.875rem]"
+    class="relative my-0 mx-auto w-screen sm:max-w-[43.0625rem] sm:px-10 md:px-0 lg:my-[5.875rem] lg:flex lg:max-w-4xl lg:gap-[1.875rem] xl:max-w-6xl"
   >
     <slot></slot>
   </div>
