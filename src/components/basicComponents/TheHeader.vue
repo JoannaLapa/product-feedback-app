9c9ca334b4f511d2d0a63d928eb991e8ecf1ffd9
Joanna <jlapa.transport@gmail.com>
2023-01-28 13:02:35 +0100

    bug fix unworking code deleted & replaced

--- src/components/basicComponents/TheHeader.vue
+++ src/components/basicComponents/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div
-      class="max-sm:fixed max-sm:w-screen top-0 flex flex-col sm:mt-14 sm:mb-10 sm:grid sm:max-h-[11.125rem] sm:grid-cols-3 sm:grid-rows-1 sm:gap-2.5 lg:my-0 lg:max-h-[33.0625rem] lg:grid-cols-1 lg:grid-rows-[8.5625rem_23rem] lg:gap-6 xl:w-63"
+      class="top-0 flex flex-col sm:mt-14 sm:mb-10 sm:grid sm:max-h-[11.125rem] sm:grid-cols-3 sm:grid-rows-1 sm:gap-2.5 lg:my-0 lg:max-h-[33.0625rem] lg:grid-cols-1 lg:grid-rows-[8.5625rem_23rem] lg:gap-6 xl:w-63"
     >
       <div
         class="lg:rows-span-1 flex items-center justify-between bg-header-sm bg-cover px-6 py-4 sm:col-span-1 sm:min-h-full sm:items-end sm:rounded-lg sm:bg-header-md sm:bg-auto sm:py-6 lg:max-h-[6.5206rem] xl:bg-header-xl"
@@ -18,11 +18,11 @@
         </button>
       </div>
       <div
-        class="max-sm:w-3/4 max-sm:before:absolute max-sm:before:-left-2/4 max-sm:before:h-screen max-sm:before:w-[50%] max-sm:before:bg-black max-sm:before:opacity-50 relative right-0 flex self-end bg-neutral-200 sm:col-start-2 sm:col-end-4 sm:flex sm:max-h-full lg:left-0 lg:col-span-1"
+        class="sm: absolute top-[72px] right-0 z-20 flex h-screen w-3/4 self-end bg-neutral-200 before:absolute before:-left-2/4 before:h-screen before:w-[50%] before:bg-black before:opacity-50 sm:static sm:col-start-2 sm:col-end-4 sm:flex sm:max-h-full sm:w-full sm:before:hidden lg:left-0 lg:col-span-1"
         :class="{ hidden: isOpened }"
       >
         <ul
-          class="max-sm:h-screen max-sm:p-6 flex flex-col gap-5 sm:grid sm:grid-cols-2 sm:grid-rows-1 sm:gap-2.5 lg:grid-cols-1 lg:grid-rows-[10.375rem_11.125rem] lg:gap-6"
+          class="flex flex-col gap-5 p-6 sm:grid sm:grid-cols-2 sm:grid-rows-1 sm:gap-2.5 sm:p-0 lg:grid-cols-1 lg:grid-rows-[10.375rem_11.125rem] lg:gap-6"
         >
           <SortByCategory />
           <RoadmapMenu />
