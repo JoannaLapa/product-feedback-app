109c1616b8841706dffaeed43227e9656964711d
Joanna <jlapa.transport@gmail.com>
2023-01-29 11:37:48 +0100

    hardcoded min-h replaced with customized

--- src/components/basicComponents/BaseBar.vue
+++ src/components/basicComponents/BaseBar.vue
@@ -2,7 +2,7 @@
   <div
     class="flex items-center justify-between py-2 px-6 sm:rounded-lg sm:py-3.5 sm:pr-3 sm:pl-6 lg:pr-4"
     :class="{
-      'min-h-[3.5rem] bg-neutral-600': variant === 'primary',
+      'min-h-14 bg-neutral-600': variant === 'primary',
     }"
   >
     <slot></slot>
