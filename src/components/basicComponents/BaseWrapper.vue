a061148594814bf9bf050d844d5d91592cbc01dd
Joanna <jlapa.transport@gmail.com>
2023-01-31 10:53:18 +0100

    comment added to fix in next branches

--- src/components/basicComponents/BaseWrapper.vue
+++ src/components/basicComponents/BaseWrapper.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- to change sm:max-w-[43.0625rem] for tailwind spacing unit in next branches where spacing already exists in tailwind.config-->
   <div class="my-0 mx-auto w-screen sm:max-w-[43.0625rem] sm:px-10 md:px-0">
     <slot></slot>
   </div>
