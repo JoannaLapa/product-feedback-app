5c627bd8dfdb4771ca399789483bbb7d3e6ac2d3
Joanna <jlapa.transport@gmail.com>
2023-02-01 18:22:48 +0100

    layout bugs fix

--- src/components/feedbacks/FeedbackItem.vue
+++ src/components/feedbacks/FeedbackItem.vue
@@ -1,25 +1,26 @@
 <template>
-  <router-link :to="feedbackPageLink">
-    <li>
-      <BaseBox>
-        <article
-          class="group grid cursor-pointer grid-cols-2 grid-rows-feedback-article gap-4 p-6 sm:grid-cols-feedback-article-sm sm:grid-rows-1 sm:gap-x-10 sm:px-8 sm:py-8 xl:py-7"
+  <li>
+    <BaseBox>
+      <article
+        class="group grid cursor-pointer grid-cols-2 grid-rows-feedback-article gap-4 p-6 sm:grid-cols-feedback-article-sm sm:grid-rows-1 sm:gap-x-10 sm:px-8 sm:py-8 xl:py-7"
+      >
+        <!-- the commented paragraph should be added when the component is used in StatusFeedbackList page -->
+        <!-- <p v-if="roadmap">{{ feedback.status }}</p> -->
+        <!-- status - activated in the Roadmap, hidden in the FeedbackList -->
+        <BaseButton
+          variant="voted"
+          :number="feedback.upvotes"
+          class="group relative col-span-1 row-start-2 flex w-fit flex-row-reverse place-content-center sm:row-span-full sm:flex-col"
         >
-          <!-- the commented paragraph should be added when the component is used in StatusFeedbackList page -->
-          <!-- <p v-if="roadmap">{{ feedback.status }}</p> -->
-          <!-- status - activated in the Roadmap, hidden in the FeedbackList -->
-          <BaseButton
-            variant="voted"
-            :number="feedback.upvotes"
-            class="group relative col-span-1 row-start-2 flex w-fit flex-row-reverse place-content-center sm:row-span-full sm:flex-col"
-          >
-            <ArrowUp
-              class="transition-300 stroke-primary-100 transition group-focus:stroke-neutral-100"
-            />
-          </BaseButton>
-          <div
-            class="col-span-full row-span-1 flex flex-col gap-2 sm:col-span-2 sm:row-span-full"
-          >
+          <ArrowUp
+            class="transition-300 stroke-primary-100 transition group-focus:stroke-neutral-100"
+          />
+        </BaseButton>
+        <router-link
+          class="col-span-full row-span-1 sm:col-span-2 sm:row-span-full"
+          :to="feedbackPageLink"
+        >
+          <div class="flex flex-col gap-2">
             <h2
               class="transition-300 text-xxs font-bold text-neutral-500 transition group-hover:text-primary-100 sm:text-lg"
             >
@@ -34,20 +35,20 @@
               </p>
             </div>
           </div>
-          <div
-            class="col-start-2 flex items-center gap-2 justify-self-end text-xxs font-bold sm:col-span-3 sm:row-span-full sm:self-center sm:text-base"
-          >
-            <IconComments />
-            <!-- if there iss no comment  - the number should be in grey color -->
-            <!-- :class="{ 'opacity-50': the condition?? }" -->
-            <p class="text-neutral-500 opacity-100">
-              {{ feedback.comments ? feedback.comments.length : 0 }}
-            </p>
-          </div>
-        </article>
-      </BaseBox>
-    </li>
-  </router-link>
+        </router-link>
+        <div
+          class="col-start-2 flex items-center gap-2 justify-self-end text-xxs font-bold sm:col-span-3 sm:row-span-full sm:self-center sm:text-base"
+        >
+          <IconComments />
+          <!-- if there iss no comment  - the number should be in grey color -->
+          <!-- :class="{ 'opacity-50': the condition?? }" -->
+          <p class="text-neutral-500 opacity-100">
+            {{ feedback.comments ? feedback.comments.length : 0 }}
+          </p>
+        </div>
+      </article>
+    </BaseBox>
+  </li>
 </template>
 
 <script setup>
@@ -57,11 +58,16 @@ import IconComments from "../icons/IconComments.vue";
 import BaseBox from "../basicComponents/BaseBox.vue";
 import BaseButton from "../basicComponents/BaseButton.vue";
 
-defineProps({
+const props = defineProps({
   // Ola's suggestion - If null then you should think about hiding this component
   feedback: {
     type: Object,
     default: null,
+  },
+  to: {
+    type: String,
+    required: false,
+    default: "/feedbacks",
   },
 });
 const feedbackPageLink = computed(() => `/${props.feedback.id}`);
