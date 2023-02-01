73a1dac152f06143d8302050da08928070ff5995
Joanna <jlapa.transport@gmail.com>
2023-02-01 11:31:59 +0100

    max-w class name changed for better readibiility

--- src/components/basicComponents/BaseWrapper.vue
+++ src/components/basicComponents/BaseWrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="sm:max-w-172 relative my-0 mx-auto w-screen sm:px-10 md:px-0 lg:my-24 lg:flex lg:max-w-4xl lg:gap-8 xl:max-w-6xl"
+    class="relative my-0 mx-auto sm:px-10 md:max-w-3xl md:px-24 lg:my-24 lg:flex lg:gap-8 lg:px-0 xl:max-w-6xl"
   >
     <slot></slot>
   </div>
