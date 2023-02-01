aabf8a04d62518879df05ddca6f066e0a2f76c71
Joanna <jlapa.transport@gmail.com>
2023-02-01 10:32:57 +0100

    comment added

--- tailwind.config.js
+++ tailwind.config.js
@@ -87,6 +87,7 @@ module.exports = {
         xs: "375px",
       },
       spacing: {
+        //follows tailwind spacing system organisation - 1 = approx 4px //
         7.5: "1.875rem",
         17: "4.375rem", //70px
         18: "4.5rem",
