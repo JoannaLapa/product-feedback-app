d2ddd4609c2d14efb34d1723f87d51e1a249c87c
Joanna <jlapa.transport@gmail.com>
2023-02-02 23:16:20 +0100

    state & actions for user sorting actions add

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,13 +1,20 @@
 import { defineStore } from "pinia";
 
 export const ADD_SELECTED_CATEGORY = "ADD_SELECTED_CATEGORY";
+export const ADD_SELECTED_SORTING_CATEGORY = "ADD_SELECTED_SORTING_CATEGORY";
 export const useUserStore = defineStore("user", {
   state: () => ({
     selectedCategories: [{ id: 0, name: "All" }],
+    selectedSortingCategory: [
+      { id: 1, name: "Most Upvotes", unavailable: false },
+    ],
   }),
   actions: {
     [ADD_SELECTED_CATEGORY](category) {
       this.selectedCategories = category;
+    },
+    [ADD_SELECTED_SORTING_CATEGORY](sortingCategory) {
+      this.selectedSortingCategory = sortingCategory;
     },
   },
 });
