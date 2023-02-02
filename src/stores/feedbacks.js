ebae797679ae1094ca3ab0c8250e2df497d04e59
Joanna <jlapa.transport@gmail.com>
2023-02-02 23:15:11 +0100

    sorting logic add

--- src/stores/feedbacks.js
+++ src/stores/feedbacks.js
@@ -7,6 +7,7 @@ export const FETCH_FEEDBACKS = "FETCH_FEEDBACKS";
 export const COUNTED_STATUS_MAP = "COUNTED_STATUS_MAP";
 export const FILTERED_FEEDBACKS = "FILTERED_FEEDBACKS";
 export const INCLUDE_FEEDBACK_BY_CATEGORY = "INCLUDE_FEEDBACK_BY_CATEGORY";
+export const SORTED_FEEDBACKS = "SORTED_FEEDBACKS";
 
 export const useFeedbacksStore = defineStore("feedbacks", {
   state: () => ({
@@ -18,6 +19,12 @@ export const useFeedbacksStore = defineStore("feedbacks", {
       { id: 3, name: "Enhancement" },
       { id: 4, name: "Bug" },
       { id: 5, name: "Feature" },
+    ],
+    options: [
+      { id: 1, name: "Most Upvotes", unavailable: false },
+      { id: 2, name: "Least Upvotes", unavailable: false },
+      { id: 3, name: "Most Comments", unavailable: false },
+      { id: 4, name: "Least Comments", unavailable: false },
     ],
   }),
   actions: {
@@ -59,7 +66,8 @@ export const useFeedbacksStore = defineStore("feedbacks", {
       if (userStore.selectedCategories.id === 0) return true;
       return userStore.selectedCategories.name === feedback.category;
     },
-    //if the feedback.category is empty I return all feedbacks - I suppose that I should pront some modal window to show thet there is any feedback at this category?
+    //if the feedback.category is empty I return all feedbacks - idea for the future improvement - maybe better is to make this button disabled
+    //filtering feedbacks when the user clicks on the button with category
     [FILTERED_FEEDBACKS](state) {
       const filteredFeedbacks = state.feedbacks.filter((feedback) =>
         this.INCLUDE_FEEDBACK_BY_CATEGORY(feedback)
@@ -67,6 +75,36 @@ export const useFeedbacksStore = defineStore("feedbacks", {
       return filteredFeedbacks.length === 0
         ? state.feedbacks
         : filteredFeedbacks;
+    },
+    //sorting feedbacks when the user chooses a sorting category - default Most Upvotes
+    [SORTED_FEEDBACKS]() {
+      const userStore = useUserStore();
+      const filteredFeedbacks = this.FILTERED_FEEDBACKS;
+      //sort by "Least Upvotes"
+      if (userStore.selectedSortingCategory.id === 2) {
+        return filteredFeedbacks.sort(
+          (feedbackA, feedbackB) => feedbackA.upvotes - feedbackB.upvotes
+        );
+        //sort by "Most Comments", if there is any comment return 0 else return the length of comments array
+      } else if (userStore.selectedSortingCategory.id === 3) {
+        return filteredFeedbacks.sort(
+          (feedbackA, feedbackB) =>
+            (feedbackB.comments ? feedbackB.comments.length : 0) -
+            (feedbackA.comments ? feedbackA.comments.length : 0)
+        );
+        //sort by "Least Comments", if there is any comment return 0 else return the length of comments array
+      } else if (userStore.selectedSortingCategory.id === 4) {
+        return filteredFeedbacks.sort(
+          (feedbackA, feedbackB) =>
+            (feedbackA.comments ? feedbackA.comments.length : 0) -
+            (feedbackB.comments ? feedbackB.comments.length : 0)
+        );
+        //sort by "Most Upvotes"
+      } else {
+        return filteredFeedbacks.sort(
+          (feedbackA, feedbackB) => feedbackB.upvotes - feedbackA.upvotes
+        );
+      }
     },
   },
 });
