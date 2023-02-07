36784a6a990d1e9a6572ae523d25dcc036a0f15e
Joanna <jlapa.transport@gmail.com>
2023-02-07 16:56:34 +0100

    commented to fix bug on deployment

--- src/pages/feedbacks/FeedbackDetail.vue
+++ src/pages/feedbacks/FeedbackDetail.vue
@@ -4,24 +4,24 @@
       <GoBack>
         <BaseButton link to="" text="Edit Feedback" />
       </GoBack>
-      <FeedbackItem :feedback="feedbacks[$route.params.id - 1]" />
+      <!-- <FeedbackItem :feedback="feedbacks[$route.params.id - 1]" />
       <CommentsList />
-      <AddComment />
+      <AddComment /> -->
     </BaseWrapper>
   </main>
 </template>
 
 <script setup>
-import { onMounted, computed } from "vue";
+import { onMounted } from "vue";
 import { useFeedbacksStore } from "../../stores/feedbacks";
 import BaseButton from "../../components/basicComponents/BaseButton.vue";
 import GoBack from "../../components/basicComponents/GoBack.vue";
-import FeedbackItem from "../../components/feedbacks/FeedBackItem.vue";
-import CommentsList from "../../components/comments/CommentsList.vue";
-import AddComment from "../../components/comments/AddComment.vue";
+// import FeedbackItem from "../../components/feedbacks/FeedBackItem.vue";
+// import CommentsList from "../../components/comments/CommentsList.vue";
+// import AddComment from "../../components/comments/AddComment.vue";
 import BaseWrapper from "../../components/basicComponents/BaseWrapper.vue";
 
 const feedbacksStore = useFeedbacksStore();
-const feedbacks = computed(() => feedbacksStore.feedbacks);
+// const feedbacks = computed(() => feedbacksStore.feedbacks);
 onMounted(feedbacksStore.FETCH_FEEDBACKS);
 </script>
