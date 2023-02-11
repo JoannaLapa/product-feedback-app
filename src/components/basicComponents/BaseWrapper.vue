d15d70b9bbd05d99f13afdfa18cb5509c1d7a53f
Joanna <jlapa.transport@gmail.com>
2023-02-11 20:09:18 +0100

    prop variant added

--- src/components/basicComponents/BaseWrapper.vue
+++ src/components/basicComponents/BaseWrapper.vue
@@ -2,7 +2,18 @@
   <!-- to change sm:max-w-[43.0625rem] for tailwind spacing unit in next branches where spacing already exists in tailwind.config-->
   <div
     class="relative my-0 mx-auto sm:px-10 md:max-w-3xl md:px-24 lg:my-24 lg:flex lg:gap-8 lg:px-0 xl:max-w-6xl"
+    :class="{ 'flex flex-col': variant === 'secondary' }"
   >
     <slot></slot>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  variant: {
+    type: String,
+    default: "primary",
+    validation: (variant) => ["primary", "secondary"].includes(variant),
+  },
+});
+</script>
