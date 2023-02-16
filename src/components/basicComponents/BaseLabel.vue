eced548170229efdfe4ebc74ddaa285f4f96136a
Joanna <jlapa.transport@gmail.com>
2023-02-16 11:34:58 +0100

    component created

---
+++ src/components/basicComponents/BaseLabel.vue
@@ -0,0 +1,29 @@
+<template>
+  <label :for="labelFor" class="text-xxs font-bold text-neutral-500">
+    {{ title }}</label
+  >
+  <p id="id" class="text-xxs text-neutral-400">
+    {{ description }}
+  </p>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  labelFor: {
+    type: String,
+    default: "",
+  },
+  idTitle: {
+    type: String,
+    default: "",
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+});
+</script>
