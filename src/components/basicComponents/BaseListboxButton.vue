8371633bf01a0187077b90a0de1be08cc010fa02
Joanna <jlapa.transport@gmail.com>
2023-02-18 08:21:30 +0100

    component created for better readibility

---
+++ src/components/basicComponents/BaseListboxButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <ListboxButton
+    v-slot="{ open }"
+    class="relative flex cursor-pointer items-center gap-2 text-xxs font-bold text-neutral-300 md:text-xs"
+  >
+    <slot></slot>
+    <ArrowUp v-show="open" class="stroke-neutral-100" />
+    <ArrowDown v-show="!open" />
+  </ListboxButton>
+</template>
+
+<script setup>
+import { ListboxButton } from "@headlessui/vue";
+import ArrowDown from "../icons/ArrowDown.vue";
+import ArrowUp from "../icons/ArrowUp.vue";
+</script>
