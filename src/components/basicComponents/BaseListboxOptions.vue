7b6adfcc24db10b4b9ced4bb965d7814fb913fd0
Joanna <jlapa.transport@gmail.com>
2023-02-20 10:48:11 +0100

    components created to better reuse BaseSelect

---
+++ src/components/basicComponents/BaseListboxOptions.vue
@@ -0,0 +1,23 @@
+<template>
+  <ListboxOptions
+    class="divide-neutral-700/15 absolute z-10 flex flex-col divide-y rounded-lg border-neutral-700 bg-neutral-100 indent-6 text-xxs shadow-3xl sm:top-14"
+    role="listbox"
+    :class="{
+      'w-60 translate-y-1 lg:w-64': variant === 'narrow',
+      'w-full translate-y-6': variant === 'wide',
+    }"
+  >
+    <slot />
+  </ListboxOptions>
+</template>
+
+<script setup>
+import { ListboxOptions } from "@headlessui/vue";
+defineProps({
+  variant: {
+    type: String,
+    required: true,
+    validation: (variant) => ["wide", "narrow"].includes(variant),
+  },
+});
+</script>
