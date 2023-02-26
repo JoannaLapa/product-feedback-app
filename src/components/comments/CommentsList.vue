c132c3fb0e45550e24efef0b8b3258b8a64e1c6a
Joanna <jlapa.transport@gmail.com>
2023-02-26 07:23:44 +0100

    props number & logic add, unworking code commented

--- src/components/comments/CommentsList.vue
+++ src/components/comments/CommentsList.vue
@@ -2,24 +2,32 @@
   <section>
     <BaseBox>
       <h2>
-        <span>{{}}</span>
-        Comments
+        <span>{{ number }}</span>
+        {{ number === 1 ? "Comment" : "Comments" }}
       </h2>
       <!-- if there is only one comments we should write in singular form -->
-      <div>
+      <!-- <div>
         <ul>
           <CommentItem>
             <PostReply />
           </CommentItem>
-          <!-- add inside comment-item v-if if there is a reply -->
-          <!-- add post-replay when the user clicks on reply -->
-        </ul>
-      </div>
+         add inside comment-item v-if if there is a reply -->
+      <!-- add post-replay when the user clicks on reply -->
+      <!--  </ul>
+      </div> -->
     </BaseBox>
   </section>
 </template>
 
 <script setup>
-import CommentItem from "./CommentItem.vue";
-import PostReply from "./PostReply.vue";
+// import CommentItem from "./CommentItem.vue";
+// import PostReply from "./PostReply.vue";
+import BaseBox from "../basicComponents/BaseBox.vue";
+
+defineProps({
+  number: {
+    type: Number,
+    required: true,
+  },
+});
 </script>
