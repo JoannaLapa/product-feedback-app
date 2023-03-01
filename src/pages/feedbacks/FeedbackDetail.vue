8a1a6f86f13ba2696729a8c0d2875d20064c0701
Joanna <jlapa.transport@gmail.com>
2023-03-01 19:54:25 +0100

    props, computed & ref added, typo fix

--- src/pages/feedbacks/FeedbackDetail.vue
+++ src/pages/feedbacks/FeedbackDetail.vue
@@ -10,24 +10,34 @@
           variant="secondary"
         />
       </GoBack>
-      <FeedbackItem :feedback="feedbacks[$route.params.id - 1]" />
-      <CommentsList :number="feedbacks[$route.params.id - 1].comments.length" />
+      <FeedbackItem
+        :feedback="feedback"
+        :number="Number(route.params.id.slice(1))"
+      />
+      <CommentsList
+        :number="feedback.comments ? feedback.comments.length : 0"
+        :comments-list="commentsList"
+      />
       <!--  <AddComment /> -->
     </BaseWrapper>
   </main>
 </template>
 
 <script setup>
-import { onMounted, computed } from "vue";
+import { computed, ref } from "vue";
 import { useFeedbacksStore } from "../../stores/feedbacks";
+import { useRoute } from "vue-router";
 import BaseButton from "../../components/basicComponents/BaseButton.vue";
 import GoBack from "../../components/basicComponents/GoBack.vue";
-import FeedbackItem from "../../components/feedbacks/FeedBackItem.vue";
+import FeedbackItem from "../../components/feedbacks/FeedbackItem.vue";
 import CommentsList from "../../components/comments/CommentsList.vue";
 // import AddComment from "../../components/comments/AddComment.vue";
 import BaseWrapper from "../../components/basicComponents/BaseWrapper.vue";
 
+const feedback = ref(feedbacks.value[Number(route.params.id.slice(1))]);
+const commentsList = ref([...feedback.value.comments]);
 const feedbacksStore = useFeedbacksStore();
-const feedbacks = computed(() => feedbacksStore.feedbacks);
-onMounted(feedbacksStore.fetchFeedbacks);
+const route = useRoute();
+feedbacksStore.fetchFeedbacks();
+const feedbacks = computed(() => feedbacksStore.sortedFeedbacksList);
 </script>
