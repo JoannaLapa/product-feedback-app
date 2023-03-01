8e59a46f0c1abe48d68fe217ddf9ad629384bb75
Joanna <jlapa.transport@gmail.com>
2023-03-01 19:38:28 +0100

    dynamic content added, comments deleted

--- src/components/comments/CommentsList.vue
+++ src/components/comments/CommentsList.vue
@@ -5,28 +5,53 @@
         <span>{{ number }}</span>
         {{ number === 1 ? "Comment" : "Comments" }}
       </h2>
-      <!-- if there is only one comments we should write in singular form -->
-      <!-- <div>
+      <div>
         <ul>
-          <CommentItem>
-            <PostReply />
+          <CommentItem
+            v-for="comment in commentsList"
+            :key="comment.id"
+            :name="comment.user.name"
+            :user-name="comment.user.username"
+            :content="comment.content"
+          >
+            <img
+              :src="`src/${comment.user.image.slice(2)}`"
+              alt="photo of the user"
+            />
+            <div v-if="comment.replies">
+              <CommentItem
+                v-for="reply in comment.replies"
+                :key="reply.content"
+                :name="reply.user.name"
+                :user-name="reply.user.username"
+                :content="reply.content"
+              >
+                <img
+                  :src="`src/${reply.user.image.slice(2)}`"
+                  alt="photo of the user"
+                />
+              </CommentItem>
+            </div>
+            <!-- <PostReply /> -->
           </CommentItem>
-         add inside comment-item v-if if there is a reply -->
-      <!-- add post-replay when the user clicks on reply -->
-      <!--  </ul>
-      </div> -->
+          <!-- add post-replay when the user clicks on reply -->
+        </ul>
+      </div>
     </BaseBox>
   </section>
 </template>
 
 <script setup>
-// import CommentItem from "./CommentItem.vue";
+import CommentItem from "./CommentItem.vue";
 // import PostReply from "./PostReply.vue";
 import BaseBox from "../basicComponents/BaseBox.vue";
-
 defineProps({
   number: {
     type: Number,
+    required: true,
+  },
+  commentsList: {
+    type: Array,
     required: true,
   },
 });
