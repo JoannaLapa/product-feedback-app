2ecccdaae87e83befef2418b834f581b2b186388
Joanna <jlapa.transport@gmail.com>
2023-03-01 19:36:40 +0100

    props added, comments deleted

--- src/components/comments/CommentItem.vue
+++ src/components/comments/CommentItem.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>
-    <!-- <img src="../assets/user-images/image-.jpg" alt="" /> -->
-    <!-- check how to add user name to img src and alt dynamically from comments.user.name -->
-    <h3>{{}}</h3>
-    <!-- here comments.user.name -->
-    <p>{{}}</p>
-    <!-- here comments.user.username -->
-    <BaseButton tag="button" type="button" variant="pure" text="Reply" />
-  </div>
-  <blockquote>{{}}</blockquote>
-  <!-- here comments.content -->
-  <!-- v-if reply add span with username who replies - replies.replyingTo -->
-  <slot></slot>
+  <li>
+    <article>
+      <div>
+        <slot></slot>
+        <h3>{{ name }}</h3>
+        <p>{{ userName }}</p>
+        <BaseButton tag="button" type="button" variant="pure" text="Reply" />
+      </div>
+      <p>{{ content }}</p>
+    </article>
+  </li>
 </template>
 
 <script setup>
 import BaseButton from "../basicComponents/BaseButton.vue";
+defineProps({
+  userName: {
+    type: String,
+    default: "",
+  },
+  name: {
+    type: String,
+    default: "",
+  },
+  content: {
+    type: String,
+    default: "",
+  },
+});
 </script>
