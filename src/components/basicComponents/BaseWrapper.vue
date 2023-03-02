287f7987f56670f7a968a02e93ea675d24c58502
Joanna <jlapa.transport@gmail.com>
2023-03-02 18:12:22 +0100

    tertiary variant add and styled

--- src/components/basicComponents/BaseWrapper.vue
+++ src/components/basicComponents/BaseWrapper.vue
@@ -5,7 +5,7 @@
       'mx-6 mt-8 flex flex-col gap-9 sm:w-134 sm:gap-17 lg:my-23':
         variant === 'secondary',
       'sm:px-10 md:px-24 lg:my-24 lg:gap-8 xl:max-w-6xl': variant === 'primary',
-      'my-6 mx-6 flex flex-col': variant === 'tertiary',
+      'my-6 mx-6 flex flex-col gap-6': variant === 'tertiary',
     }"
   >
     <slot></slot>
