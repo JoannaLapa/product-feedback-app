b83497e8c9a08dd914816c9e7e3e033ea7a26f55
Joanna <jlapa.transport@gmail.com>
2023-03-03 11:20:20 +0100

    img moved to CommentItem, styles added

--- src/components/comments/CommentsList.vue
+++ src/components/comments/CommentsList.vue
@@ -1,46 +1,46 @@
 <template>
-  <section>
-    <BaseBox>
-      <h2>
-        <span>{{ number }}</span>
-        {{ number === 1 ? "Comment" : "Comments" }}
-      </h2>
-      <div>
-        <ul>
-          <CommentItem
-            v-for="comment in commentsList"
-            :key="comment.id"
-            :name="comment.user.name"
-            :user-name="comment.user.username"
-            :content="comment.content"
+  <!-- future todo- number should be equal to comments + replies not only the comments -->
+  <BaseBox variant="secondary">
+    <h2 class="text-lg font-bold text-neutral-500">
+      <span>{{ number }}</span>
+      {{ number === 1 ? "Comment" : "Comments" }}
+    </h2>
+    <div>
+      <ul class="divide-neutral-700/15 divide-y">
+        <CommentItem
+          v-for="comment in commentsList"
+          :key="comment.id"
+          :name="comment.user.name"
+          :user-name="comment.user.username"
+          :content="comment.content"
+          :src="`src/${comment.user.image.slice(2)}`"
+          variant="primary"
+        >
+          <!-- future todo: set before with same tyles like divide from comment up, but on x and from the begginind of comment content to the middle of avatar of the last reply?  -->
+          <ul
+            v-if="comment.replies"
+            class="relative flex flex-col gap-6 divide-neutral-400/10 pl-6 before:absolute before:left-0 before:h-4/6 before:border-l before:border-neutral-400 before:opacity-10"
           >
-            <img
-              :src="`src/${comment.user.image.slice(2)}`"
-              alt="photo of the user"
-            />
-            <ul v-if="comment.replies">
-              <CommentItem
-                v-for="reply in comment.replies"
-                :key="reply.content"
-                :name="reply.user.name"
-                :user-name="reply.user.username"
-                :content="reply.content"
-              >
-                <img
-                  :src="`src/${reply.user.image.slice(2)}`"
-                  alt="photo of the user"
-                />
-                <!-- <PostReply /> -->
-                <!-- add post-replay when the user clicks on reply -->
-              </CommentItem>
-            </ul>
-            <!-- <PostReply /> -->
-            <!-- add post-replay when the user clicks on reply -->
-          </CommentItem>
-        </ul>
-      </div>
-    </BaseBox>
-  </section>
+            <CommentItem
+              v-for="reply in comment.replies"
+              :key="reply.content"
+              :name="reply.user.name"
+              :user-name="reply.user.username"
+              :content="reply.content"
+              :src="`src/${reply.user.image.slice(2)}`"
+              reply
+              :reply-to="`@${reply.replyingTo} `"
+            >
+              <!-- <PostReply /> -->
+              <!-- add post-replay when the user clicks on reply -->
+            </CommentItem>
+          </ul>
+          <!-- <PostReply /> -->
+          <!-- add post-replay when the user clicks on reply -->
+        </CommentItem>
+      </ul>
+    </div>
+  </BaseBox>
 </template>
 
 <script setup>
