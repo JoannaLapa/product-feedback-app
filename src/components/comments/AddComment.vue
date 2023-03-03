49408b5c013d82ef926d0a8595f4955696aab8fa
Joanna <jlapa.transport@gmail.com>
2023-03-03 11:18:25 +0100

    mobile styles add

--- src/components/comments/AddComment.vue
+++ src/components/comments/AddComment.vue
@@ -1,21 +1,30 @@
 <template>
-  <BaseBox>
-    <form>
-      <label for="comment">Add comment:</label>
-      <textarea
-        id="comment"
-        name="comment"
-        maxlength="250"
-        placeholder="Type your comment here"
-        aria-describedby="characters-left"
-      />
-      <p id="characters-left">Dynamic number characters left</p>
-      <BaseButton
-        tag="button"
-        type="button"
-        variant="primary"
-        text="Post comment"
-      />
+  <BaseBox variant="primary">
+    <form class="flex flex-col gap-6">
+      <label for="comment" class="text-lg font-bold text-neutral-500"
+        >Add comment:</label
+      >
+      <div class="flex flex-col gap-4">
+        <textarea
+          id="comment"
+          name="comment"
+          maxlength="250"
+          placeholder="Type your comment here"
+          aria-describedby="characters-left"
+          class="resize-none rounded-md bg-neutral-200 p-4 placeholder:text-xxs placeholder:text-neutral-1000"
+        />
+        <div class="flex w-full justify-between">
+          <p id="characters-left" class="text-xxs text-neutral-400">
+            {{ number }} characters left
+          </p>
+          <BaseButton
+            tag="button"
+            type="button"
+            variant="primary-narrow"
+            text="Post comment"
+          />
+        </div>
+      </div>
     </form>
   </BaseBox>
 </template>
@@ -25,4 +34,11 @@
 <script setup>
 import BaseButton from "../basicComponents/BaseButton.vue";
 import BaseBox from "../basicComponents/BaseBox.vue";
+
+defineProps({
+  number: {
+    type: Number,
+    default: 250,
+  },
+});
 </script>
