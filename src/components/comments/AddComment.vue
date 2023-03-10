23d2de6271f232e8e23f8e857d076afd4faeda25
Joanna <jlapa.transport@gmail.com>
2023-03-10 12:10:46 +0100

    refactor code for better reusability

--- src/components/comments/AddComment.vue
+++ src/components/comments/AddComment.vue
@@ -1,27 +1,39 @@
 <template>
-  <BaseBox variant="primary">
-    <form class="flex flex-col gap-6">
-      <label for="comment" class="text-lg font-bold text-neutral-500"
+  <BaseBox :variant="baseBoxVariant">
+    <form class="flex w-full flex-col gap-6">
+      <label
+        v-if="postComment"
+        for="comment"
+        class="text-lg font-bold text-neutral-500"
         >Add comment:</label
       >
-      <div class="flex flex-col gap-4">
+      <div
+        class="flex flex-col gap-4"
+        :class="{
+          'justify-between sm:w-full sm:flex-row': variant === 'flex-row',
+        }"
+      >
         <textarea
           id="comment"
           name="comment"
           maxlength="250"
           placeholder="Type your comment here"
           aria-describedby="characters-left"
-          class="cursor-pointer resize-none rounded-md bg-neutral-200 p-4 text-xxs text-neutral-500 placeholder:text-xxs placeholder:text-neutral-1000 sm:text-xs"
+          class="w-full cursor-pointer resize-none rounded-md bg-neutral-200 p-4 text-xxs text-neutral-500 placeholder:text-xxs placeholder:text-neutral-1000 sm:text-xs"
         />
         <div class="flex w-full justify-between">
-          <p id="characters-left" class="text-xxs text-neutral-400">
+          <p
+            v-if="postComment"
+            id="characters-left"
+            class="text-xxs text-neutral-400"
+          >
             {{ number }} characters left
           </p>
           <BaseButton
             tag="button"
             type="button"
             variant="primary-narrow"
-            text="Post comment"
+            :text="primaryButtonText"
           />
         </div>
       </div>
@@ -34,11 +46,23 @@
 <script setup>
 import BaseButton from "../basicComponents/BaseButton.vue";
 import BaseBox from "../basicComponents/BaseBox.vue";
+import { inject } from "vue";
 
 defineProps({
   number: {
     type: Number,
     default: 250,
   },
+  postComment: {
+    type: Boolean,
+    default: false,
+  },
+  variant: {
+    type: String,
+    default: "",
+    validation: (variant) => ["flex-row", "flex-col"].includes(variant),
+  },
 });
+const baseBoxVariant = inject("baseBoxVariant");
+const primaryButtonText = inject("primaryButtonText");
 </script>
