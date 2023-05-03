2cd7fa76aa666dea171a689846acedd4e9a28134
Joanna <jlapa.transport@gmail.com>
2023-05-03 14:25:11 +0200

    primary-high variant added

--- src/components/basicComponents/BaseBar.vue
+++ src/components/basicComponents/BaseBar.vue
@@ -4,6 +4,8 @@
     :class="{
       'min-h-14 bg-neutral-600 py-2 px-3 xs:px-6 sm:py-3.5 sm:pr-3 sm:pl-6 lg:pr-4':
         variant === 'primary',
+      'min-h-25 bg-neutral-600 py-2 px-3 xs:px-6 sm:py-3.5 sm:pr-3 sm:pl-6 lg:pr-4':
+        variant === 'primary-high',
       'px-0 py-0': variant === 'neutral',
     }"
   >
