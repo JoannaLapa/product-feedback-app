14d3fb64f38204e07857286860511f4ba0b8f206
Joanna <jlapa.transport@gmail.com>
2023-05-03 12:41:22 +0200

    TabComponent & TabsWrapper created

---
+++ src/components/basicComponents/TabComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <li v-show="title === selectedTitle">
+    <slot></slot>
+  </li>
+</template>
+
+<script setup>
+import { inject } from "vue";
+defineProps({
+  title: {
+    type: String,
+    default: "Tab",
+  },
+});
+
+const selectedTitle = inject("selectedTitle");
+</script>
---
+++ src/components/basicComponents/TabsWrapper.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="title in tabTitles"
+        :key="title"
+        @click="selectedTitle = title.name"
+      >
+        {{ title.name }} ({{ title.number }})
+      </li>
+    </ul>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { ref, provide } from "vue";
+
+const props = defineProps({
+  tabTitles: {
+    type: Array,
+    required: true,
+  },
+});
+
+const selectedTitle = ref(props.tabTitles[1].name);
+provide("selectedTitle", selectedTitle);
+</script>
