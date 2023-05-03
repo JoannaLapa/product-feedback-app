2857109b337c25fabd058956457b32d8ca89b874
Joanna <jlapa.transport@gmail.com>
2023-05-03 19:28:50 +0200

    layout fix

--- src/components/basicComponents/BaseWrapper.vue
+++ src/components/basicComponents/BaseWrapper.vue
@@ -1,8 +1,9 @@
 <template>
   <div
-    class="relative my-0 mx-auto md:max-w-3xl lg:flex lg:px-0"
+    class="relative my-0 mx-auto lg:flex lg:px-0"
     :class="{
-      'sm:px-10 lg:my-24 lg:gap-8 lg:px-5 xl:max-w-6xl': variant === 'primary',
+      'sm:px-10 md:max-w-3xl lg:my-24 lg:gap-8 lg:px-5 xl:max-w-6xl':
+        variant === 'primary',
       'mx-6 mt-8 flex flex-col gap-9 sm:w-134 sm:gap-17 lg:my-23':
         variant === 'secondary',
       'my-6 mx-6 flex flex-col gap-6 md:my-14 md:max-w-2xl xl:my-20 2xl:max-w-1/2':
--- src/components/basicComponents/TabsWrapper.vue
+++ src/components/basicComponents/TabsWrapper.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <ul class="flex">
+    <ul class="grid grid-cols-3 justify-items-center">
       <li
         v-for="title in tabTitles"
         :key="title"
+        class="w-full"
         @click="selectedTitle = title.name"
       >
-        {{ title.name }} ({{ title.number }})
+        <div
+          class="flex min-h-15 font-bold text-neutral-500"
+          :class="
+            title.name === selectedTitle
+              ? 'justify-end border-b-4 border-primary-200'
+              : 'justify-center border-b-2 border-neutral-1000 pt-5 pb-4 opacity-40'
+          "
+        >
+          <p>{{ title.name }} ({{ title.number }})</p>
+        </div>
       </li>
     </ul>
     <slot />
