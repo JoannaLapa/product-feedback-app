df7ea3bd55b9022e4ddb22234e37074a6ed61627
Joanna <jlapa.transport@gmail.com>
2023-05-26 09:50:20 +0200

    onMounted defined

--- src/pages/feedbacks/FeedbackList.vue
+++ src/pages/feedbacks/FeedbackList.vue
@@ -73,7 +73,7 @@ how to do it) - I think it was a comment for resolving noFeedback case
   </BaseWrapper>
 </template>
 <script setup>
-import { computed, provide } from "vue";
+import { computed, provide, onMounted } from "vue";
 import { useFeedbacksStore } from "../../stores/feedbacks";
 import { useUserStore } from "../../stores/user.js";
 import TheHeader from "../../components/basicComponents/TheHeader.vue";
